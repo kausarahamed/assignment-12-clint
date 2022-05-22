8e192e33b4be41feaef5db3d613a7d94d614a5b1
kausarahamed <galibhasan517@gmail.com>
2022-05-23 00:58:31 +0600

    banner

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "./components/Auth/Login/Login";
 import Register from "./components/Auth/Login/Register";
-import Banner from "./components/Home/Banner";
+import Home from "./components/Home/Home";
 
 import Navber from "./components/Home/Navber";
 
@@ -10,8 +10,9 @@ function App() {
   return (
     <div>
       <Navber></Navber>
-      <Banner></Banner>
+
       <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
         <Route path="/login" element={<Login></Login>}></Route>
         <Route path="/register" element={<Register></Register>}></Route>
       </Routes>
