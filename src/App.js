0ad1e1a8e9152ec635102fe02617e0fbcc168ce0
kausarahamed <galibhasan517@gmail.com>
2022-05-23 08:54:55 +0600

    footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "./components/Auth/Login/Login";
 import Register from "./components/Auth/Login/Register";
+import Footer from "./components/Footer/Footer";
 import Home from "./components/Home/Home";
 
 import Navber from "./components/Home/Navber";
@@ -16,6 +17,7 @@ function App() {
         <Route path="/login" element={<Login></Login>}></Route>
         <Route path="/register" element={<Register></Register>}></Route>
       </Routes>
+      <Footer></Footer>
     </div>
   );
 }
